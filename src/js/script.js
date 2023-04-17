48add6b03668640a07e28066f764435dc2ac0f8f
Simenf05 <106535609+Simenf05@users.noreply.github.com>
2023-04-17 12:52:50 +0200

    funker

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,9 @@
 const backgroundInpEl = document.getElementById("backgroundColorInp")
-
+const htmlTypeEl = document.getElementById("typeHtmlElement")
+const innerHtmlEl = document.getElementById("innerHtmlChanger")
+const positionHtmlEl = document.getElementById("htmlPosition")
+const positionTextEl = document.getElementById("positionText")
+const htmlSubmitEl = document.getElementById("htmlSubmit")
 
 const htmlElementData = {
     elementType: null,
@@ -9,15 +13,69 @@ const htmlElementData = {
 
 function setUp() {
 
-    backgroundInpEl.onchange = changeBGColor
+    backgroundInpEl.onchange = e => document.body.style.backgroundColor = e.target.value
+
+    htmlTypeEl.onchange = htmlTypeChange
+    innerHtmlEl.onchange = htmlInnerTextChanger
+    positionHtmlEl.onclick = changePosition
+    htmlSubmitEl.onclick = submit
 
 }
 
+function htmlTypeChange(e) {
+    htmlElementData.elementType = e.target.value
+    innerHtmlEl.disabled = false
+}
 
+function htmlInnerTextChanger(e) {
+    const text = e.target.value
 
+    if (text === "") {
+        positionHtmlEl.disabled = true
+        return
+    }
 
-function changeBGColor(e) {
-    document.body.style.backgroundColor = e.target.value
+    htmlElementData.innerText = text
+    positionHtmlEl.disabled = false
+}
+
+function changePosition() {
+    positionTextEl.innerText = "click somewhere"
+    htmlElementData.position = [null, null]
+    htmlSubmitEl.disabled = true
+
+    let firstClick = true
+
+    document.onclick = e => {
+
+        if (firstClick) {
+            firstClick = false
+            return
+        }
+
+        const xy = htmlElementData.position
+
+        xy[0] = e.clientX
+        xy[1] = e.clientY
+
+        positionTextEl.innerText = `(${xy.toLocaleString()})`
+        console.log(htmlElementData)
+        document.onclick = null
+        htmlSubmitEl.disabled = false
+    }
+}
+
+function submit(e) {
+
+    const newEl = document.createElement(htmlElementData.elementType)
+
+    newEl.innerHTML = htmlElementData.innerText
+
+    newEl.style.position = "absolute"
+    newEl.style.left = htmlElementData.position[0]
+    newEl.style.top = htmlElementData.position[1]
+
+    document.body.appendChild(newEl)
 }
 
 setUp()
